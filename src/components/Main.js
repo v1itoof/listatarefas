d672446f7538fd83ec11b9793a0f1458eaf45103
v1itoof <joaov1c@outlook.com>
2024-03-19 10:02:20 -0300

    Novo projeot

---
+++ src/components/Main.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+import { FaPlus, FaEdit, FaTrash } from 'react-icons/fa';
+
+import './Main.css';
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+    tarefas: [],
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novaTarefas = [...tarefas];
+
+    this.setState({
+      tarefas: [...novaTarefas, novaTarefa],
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
+  render() {
+    const { novaTarefa, tarefas } = this.state;
+
+    return (
+      <div className="main">
+        <h1>Lista de Tarefas</h1>
+
+        <form onSubmit={this.handleSubmit} action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
+        </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              {tarefa}
+              <span>
+                <FaEdit className="edit" />
+                <FaTrash className="delete" />
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
